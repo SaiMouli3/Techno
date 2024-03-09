e55902280d1a5e031876d2f1b7ff09ddff5d8634
Ajay Jayaprakash <ajayjpd4@gmail.com>
2024-03-09 22:48:48 +0530

    Added Employee Efficiency logic and form

---
+++ techno/webapp/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Employee, Tool, Job
+
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ['emp_ssn', 'emp_name', 'mobile', 'address']
+
+
+class ToolForm(forms.ModelForm):
+    class Meta:
+        model = Tool
+        fields = ['tool_code', 'tool_name', 'max_length', 'cost', 'length_cut']
+
+
+class JobForm(forms.ModelForm):
+    # tool_code = forms.ModelMultipleChoiceField(queryset=Tool.objects.all(), widget=forms.CheckboxSelectMultiple)
+    class Meta:
+        model = Job
+        fields = ['job_id', 'job_name', 'length', 'no_of_holes', 'tool_code']
+
+
+
+class EmployeessnForm(forms.Form):
+    emp_ssn = forms.CharField(label='Employee SSN', max_length=20)
+
+
+
---
+++ techno/webapp/views.py
@@ -0,0 +1,156 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import EmployeeForm, ToolForm, JobForm, EmployeessnForm
+from .models import Employee, Tool, Job,Performs
+from django.db.models import Sum
+
+
+
+def employee_create_view(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/webapp/success_page/')
+    else:
+        form = EmployeeForm()
+
+    return render(request, 'webapp/employee_form.html', {'form': form})
+
+
+def tool_create_view(request):
+    if request.method == 'POST':
+        form = ToolForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/webapp/success_page/')
+    else:
+        form = ToolForm()
+
+    return render(request, 'webapp/tool_form.html', {'form': form})
+
+
+def job_create_view(request):
+    if request.method == 'POST':
+        form = JobForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/webapp/success_page/')
+    else:
+        form = JobForm()
+
+    return render(request, 'webapp/job_form.html', {'form': form})
+
+
+def employee_delete_view(request, emp_ssn):
+    employee = get_object_or_404(Employee, emp_ssn=emp_ssn)
+
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('/webapp/success_page/')
+
+    return render(request, 'webapp/employee_delete.html', {'employee': employee})
+
+
+def tool_delete_view(request, tool_code):
+    tool = get_object_or_404(Tool, tool_code=tool_code)
+
+    if request.method == 'POST':
+        tool.delete()
+        return redirect('/webapp/success_page/')
+
+    return render(request, 'webapp/tool_delete.html', {'tool': tool})
+
+
+def job_delete_view(request, job_id):
+    job = get_object_or_404(Job, job_id=job_id)
+
+    if request.method == 'POST':
+        job.delete()
+        return redirect('/webapp/success_page/')
+
+    return render(request, 'webapp/job_delete.html', {'job': job})
+
+
+def employee_update_view(request, emp_ssn):
+    employee = get_object_or_404(Employee, emp_ssn=emp_ssn)
+
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('/webapp/success_page/')
+    else:
+        form = EmployeeForm(instance=employee)
+
+    return render(request, 'webapp/employee_update.html', {'form': form})
+
+
+def tool_update_view(request, tool_code):
+    tool = get_object_or_404(Tool, tool_code=tool_code)
+
+    if request.method == 'POST':
+        form = ToolForm(request.POST, instance=tool)
+        if form.is_valid():
+            form.save()
+            return redirect('/webapp/success_page/')
+    else:
+        form = ToolForm(instance=tool)
+
+    return render(request, 'webapp/tool_update.html', {'form': form})
+
+
+def job_update_view(request, job_id):
+    job = get_object_or_404(Job, job_id=job_id)
+
+    if request.method == 'POST':
+        form = JobForm(request.POST, instance=job)
+        if form.is_valid():
+            form.save()
+            return redirect('/webapp/success_page/')
+    else:
+        form = JobForm(instance=job)
+
+    return render(request, 'webapp/job_update.html', {'form': form})
+
+
+def success_page(request):
+    return render(request, 'webapp/success_page.html')
+
+
+def calculate_efficiency(emp_ssn):
+    performs_aggregated = Performs.objects.filter(emp_ssn=emp_ssn).aggregate(
+        shift_duration_sum=Sum('shift_duration'),
+        partial_shift_sum=Sum('partial_shift'),
+        target_sum=Sum('target'),
+        achieved_sum=Sum('achieved')
+    )
+
+    shift_duration_sum = performs_aggregated['shift_duration_sum']
+    partial_shift_sum = performs_aggregated['partial_shift_sum']
+    target_sum = performs_aggregated['target_sum']
+    achieved_sum = performs_aggregated['achieved_sum']
+
+    if shift_duration_sum and partial_shift_sum and target_sum and achieved_sum:
+        x = target_sum * (partial_shift_sum / shift_duration_sum)
+        efficiency = achieved_sum / x
+        return efficiency
+    else:
+        return None
+
+def efficiency_view(request):
+    efficiency = None
+
+    if request.method == 'POST':
+        form = EmployeessnForm(request.POST)
+        if form.is_valid():
+            emp_ssn = form.cleaned_data['emp_ssn']
+            efficiency = calculate_efficiency(emp_ssn)*100
+    else:
+        form = EmployeessnForm()
+
+    return render(request, 'webapp/efficiency_template.html', {'form': form, 'efficiency': efficiency})
+
+
+    
+
+
